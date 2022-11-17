349c65c5ac765d3174cf5a373a40274bdfc210b1
aakzsh <aakashferrari@gmail.com>
2022-11-17 16:58:45 +0530

    add remaining questions

--- lib/data/questions.dart
+++ lib/data/questions.dart
@@ -28,21 +28,155 @@ List<Map<dynamic, dynamic>> round2 = [
   },
 ];
 
-List<Map<dynamic, dynamic>> round1g1 = [
+List<Map<dynamic, dynamic>> round1gA = [
   {
-    "clue": "",
+    "clue":
+        "I'm the King of angel\nUse your  silly mind \nCause you are student",
+    "code": "",
+    "answer": "Student Parking",
+  },
+  {
+    "clue":
+        "Park park park \nI'm the park \nNo no no not the main character\nBut you like if you drive near me.",
+    "code": "",
+    "answer": "Audi near Children's Park",
+  },
+  {
+    "clue":
+        "mere paas do hee shabd hai,\nlekin laakhon ke bhaavanaen hain,\nmain social media nahin hoon,\nlekin har jagah rehta hoon\nanumaan lagao ki main kaun hoon?",
+    "code": "",
+    "answer": "Post Office",
+  },
+  {
+    "clue":
+        "I've power to change the colour of marbles and\nfill your tummy to make you powerful\nGuess my name?",
+    "code": "",
+    "answer": "Rain Water Harvesting",
+  },
+  {
+    "clue":
+        "  Kitabein bhaut si pdhi hongi tumne magar kbhi kamre me baithkr pdha hai????",
+    "code": "",
+    "answer": "Reading Room",
+  },
+];
+
+List<Map<dynamic, dynamic>> round1gB = [
+  {
+    "clue":
+        "Yum yum yummy \nTum tum tummy\nBut you have to go high \nto fulfill you khwahish",
+    "code": "",
+    "answer": "Canteen (1st Floor)",
+  },
+  {
+    "clue":
+        "Everybody like to be the central happiness in their team \nI'm the central inventory of mine.",
+    "code": "",
+    "answer": "Kendriya Bhandar",
+  },
+  {
+    "clue":
+        "Word related to yard \nAnd work related to game \nNow tell my name !!",
+    "code": "",
+    "answer": "Lawn Tennis Area",
+  },
+  {
+    "clue":
+        "Aaj khush toh bhaut hoge tum \nKyuki jodiyan toh swarg me bnti hai aur uss swarg ki dharti pe khade ho tum ",
+    "code": "",
+    "answer": "GGSIPU Temple",
+  },
+  {
+    "clue":
+        "Head of your class\nHead of all class\nOnly allowance for their car",
+    "code": "",
+    "answer": "Staff Parking",
+  },
+];
+
+List<Map<dynamic, dynamic>> round1gC = [
+  {
+    "clue":
+        "I'm simple I'm common\nDon't do partiality with anyone\nYou come they come we come \nAll can sit and stay together.",
+    "code": "",
+    "answer": "Common Room",
+  },
+  {
+    "clue": "Semi zone , quarter zone \nBut don't worry I'm your safe zone",
+    "code": "",
+    "answer": "Student Sitting Zone",
+  },
+  {
+    "clue":
+        "I'm not refreshner but my etiquettes are good,\nbad for others and fly like Birds.\nHave Heart Spreads To Miles Of The Garden.",
+    "code": "",
+    "answer": "Badminton Court",
+  },
+  {
+    "clue":
+        "Na Desi hu na sanskari hu fir bhi bdi bdi jagah\npr milti hu 3rd floor hai mera ghar kamre p daalo\nna nazar Btao kon hu me.",
     "code": "",
     "answer": "",
   },
   {
-    "clue": "",
+    "clue":
+        "You can go up and down but not left and right\nI am a place of tension reliever of thousands of \nScholars.",
     "code": "",
-    "answer": "",
+    "answer": "Library Stairs",
+  },
+];
+
+List<Map<dynamic, dynamic>> round1gD = [
+  {
+    "clue":
+        "Lots of children with different species lives\nat  one place with peace and greenary.",
+    "code": "",
+    "answer": "GGSIPU GreenHouse",
   },
   {
-    "clue": "",
+    "clue":
+        "Dikhta hai nazara sara yha se , mere niche 4 dabbe samne dekhoge to dikhega andar ane k dwar piche dekhoge to dikhega sabke rhne k dwar ",
     "code": "",
-    "answer": "",
+    "answer": "Canteen Bridge",
+  },
+  {
+    "clue":
+        "My owner filled 14000 l of water everyday and it becomes 15000 l . I don't know how ",
+    "code": "",
+    "answer": "Swimming Pool Changing Room",
+  },
+  {
+    "clue":
+        "Manzil ko aashma aur mehnat ki rahe rakhte hai , apne abhiney se aveksahyo ki adakari jawa rkhte hai ",
+    "code": "",
+    "answer": "Aveksha Practice Ground",
+  },
+  {
+    "clue":
+        "A place where kids are taken care of like plants and plants are treated as small kids ",
+    "code": "",
+    "answer": "GGSIPU Nursery",
+  },
+];
+
+List<Map<dynamic, dynamic>> round1gE = [
+  {
+    "clue":
+        "When u come inside from the way to college you regularly saw me . I contain all the information and help you to reach your destination. Don't forget to say thankyou to me",
+    "code": "",
+    "answer": "College Map",
+  },
+  {
+    "clue":
+        "I'm not refreshner but my etiquettes are good,\nbad for others and fly like Birds.\nHave Heart Spreads To Miles Of The Garden.",
+    "code": "",
+    "answer": "Badminton Court",
+  },
+  {
+    "clue":
+        "Padhne ke shaukeen ki jagah hu me\nLadne ke shaukeen ki jagah hu me ",
+    "code": "",
+    "answer": "Law Library",
   },
   {
     "clue": "",
