45254261a1597dd4d936ad7f55048cfee327b977
aakzsh <aakashferrari@gmail.com>
2022-11-17 16:30:36 +0530

    add round 2 questions

---
+++ lib/data/questions.dart
@@ -0,0 +1,57 @@
+List<Map<dynamic, dynamic>> round2 = [
+  {
+    "clue": "Find out the best office from\nABCDEFWDEWBCSDWDSW",
+    "code": "",
+    "answer": "DSW Office",
+  },
+  {
+    "clue": "Annu is Echoing in ground with harbajan Singh",
+    "code": "",
+    "answer": "Cricket/Anugoonj Ground",
+  },
+  {
+    "clue": "Gas Pipeline in Front of E-Block",
+    "code": "",
+    "answer": "line of pipe,\nof gasline fro and fro\nof Block E.",
+  },
+  {
+    "clue":
+        "Residents calling on video call in residential ground and the name of this college loud out.",
+    "code": "",
+    "answer": "GGSIPU named in ground near VC Residence",
+  },
+  {
+    "clue":
+        "Here-there-everywhere\nThis way that way everyway\nAt the mall,at the showroom\nAt the hotel , at the hostel \nWhich place I'm???",
+    "code": "",
+    "answer": "Hostel Parking",
+  },
+];
+
+List<Map<dynamic, dynamic>> round1g1 = [
+  {
+    "clue": "",
+    "code": "",
+    "answer": "",
+  },
+  {
+    "clue": "",
+    "code": "",
+    "answer": "",
+  },
+  {
+    "clue": "",
+    "code": "",
+    "answer": "",
+  },
+  {
+    "clue": "",
+    "code": "",
+    "answer": "",
+  },
+  {
+    "clue": "",
+    "code": "",
+    "answer": "",
+  },
+];
